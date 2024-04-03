4edcd95e30ab9aeb61775958743415ece84227f8
KennedyRibeiro <kennedydeveloper1@gmail.com>
2024-04-03 17:42:26 -0300

    mostrar n° maior

---
+++ maiorq.py
@@ -0,0 +1,9 @@
+
+
+n1 = float(input("Qual é o primeiro número?"))
+n2 = float(input("Qual é o segundo número?"))
+
+if n1 > n2:
+    print("O maior númeor é: ",n1)
+else:
+    print("O maior número é: ",n2)
