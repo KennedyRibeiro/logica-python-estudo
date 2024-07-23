4d19505f40ef13656c820c15835ffeff41c0dd11
KennedyRibeiro <kennedydeveloper1@gmail.com>
2024-07-23 19:36:37 -0300

    prog3 list2 feito

--- lista_ex 2/prog2.py
+++ lista_ex 2/prog2.py
@@ -2,13 +2,8 @@ import math
 
 numero = float(input("Digite o número desejado: "))
 
-
 if numero > 0:
     raiz = math.sqrt(numero)
     print("A raiz do número é: ", raiz)
 else:
     print("Número inválido")
-    
-
-
-    
--- lista_ex 2/prog3.py
+++ lista_ex 2/prog3.py
@@ -1,3 +1,10 @@
+import math
 
-numero = int(input("Escolha um número inteiro: "))
+numero = float(input("Digite o número desejado: "))
 
+if numero > 0:
+    raiz = math.sqrt(numero)
+    print("A raiz do número é: ", raiz)
+else:
+    quadrado = numero ** 2
+    print("Número ao quadrado é: ", quadrado)
